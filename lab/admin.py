91f0cd8f102b84968e141ae1c92aa38786482ed4
catmanti <ssomaratne@gmail.com>
2025-03-24 19:56:18 +0530

    Add Admin site & register models

--- lab/admin.py
+++ lab/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    District,
+    MOHArea,
+    PHIArea,
+    FoodCategory,
+    FoodType,
+    Parameter,
+    FoodParameter,
+)
 
-# Register your models here.
+admin.site.register(District)
+admin.site.register(MOHArea)
+admin.site.register(PHIArea)
+admin.site.register(FoodCategory)
+admin.site.register(FoodType)
+admin.site.register(Parameter)
+admin.site.register(FoodParameter)
